0939381e87c9b646e185923cc4ee468759569003
PabloSz <pablosaez1995@gmail.com>
2019-02-11 15:47:32 -0300

    fix babel bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,108 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
-const AssetsPlugin = require('assets-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const AssetsPlugin = require("assets-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const env = process.env.NODE_ENV || 'development';
-const isDev = env === 'development';
+const env = process.env.NODE_ENV || "development";
+const isDev = env === "development";
 const isProd = !isDev;
 
-const fileSuffix = isDev ? '' : '-[chunkhash].min';
+const fileSuffix = isDev ? "" : "-[chunkhash].min";
 
 function resolveModulePath(name) {
-  const packageJson = '/package.json';
+  const packageJson = "/package.json";
   return path.dirname(require.resolve(`${name}${packageJson}`));
 }
 
-const codemirrorPath = resolveModulePath('codemirror');
-const bootstrapPath = resolveModulePath('bootstrap');
+const codemirrorPath = resolveModulePath("codemirror");
+const bootstrapPath = resolveModulePath("bootstrap");
 
 module.exports = {
   entry: {
-    index: './lib/scripts/index.js',
-    database: './lib/scripts/database.js',
-    collection: './lib/scripts/collection.js',
-    document: './lib/scripts/document.js',
-    gridfs: './lib/scripts/gridfs.js',
+    index: "./lib/scripts/index.js",
+    database: "./lib/scripts/database.js",
+    collection: "./lib/scripts/collection.js",
+    document: "./lib/scripts/document.js",
+    gridfs: "./lib/scripts/gridfs.js",
 
-    vendor: './lib/scripts/vendor.js',
-    codemirror: './lib/scripts/codeMirrorLoader.js',
+    vendor: "./lib/scripts/vendor.js",
+    codemirror: "./lib/scripts/codeMirrorLoader.js",
   },
   output: {
     filename: `[name]${fileSuffix}.js`,
-    path: path.join(__dirname, 'build'),
-    publicPath: 'public/',
+    path: path.join(__dirname, "build"),
+    publicPath: "public/",
   },
 
   module: {
-    loaders: [
+    rules: [
       {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules)/,
-        query: {
-          presets: ['es2015'],
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
     ],
   },
 
   plugins: [
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"]),
 
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env),
+      "process.env.NODE_ENV": JSON.stringify(env),
       __DEV__: isDev,
     }),
 
     new webpack.optimize.CommonsChunkPlugin({
-      names: ['codemirror', 'vendor'],
+      names: ["codemirror", "vendor"],
       filename: `[name]${fileSuffix}.js`,
     }),
 
-    isProd && (new webpack.optimize.UglifyJsPlugin()),
-    isProd && (new webpack.optimize.OccurrenceOrderPlugin(true)),
+    isProd && new webpack.optimize.UglifyJsPlugin(),
+    isProd && new webpack.optimize.OccurrenceOrderPlugin(true),
 
     new CopyWebpackPlugin([
-      { from: 'public/images/*', to: 'img/[name].[ext]' },
-      { from: 'public/stylesheets/*', to: 'css/[name].[ext]' },
+      { from: "public/images/*", to: "img/[name].[ext]" },
+      { from: "public/stylesheets/*", to: "css/[name].[ext]" },
 
-      { from: path.join(codemirrorPath, '/lib/codemirror.css'), to: 'css/[name].[ext]' },
-      { from: path.join(codemirrorPath, '/theme/*'), to: 'css/theme/[name].[ext]' },
+      {
+        from: path.join(codemirrorPath, "/lib/codemirror.css"),
+        to: "css/[name].[ext]",
+      },
+      {
+        from: path.join(codemirrorPath, "/theme/*"),
+        to: "css/theme/[name].[ext]",
+      },
 
-      { from: path.join(bootstrapPath, '/dist/fonts/*'), to: 'fonts/[name].[ext]' },
-      { from: path.join(bootstrapPath, '/dist/css/bootstrap.min.css'), to: 'css/[name].[ext]' },
-      { from: path.join(bootstrapPath, '/dist/css/bootstrap.min.css.map'), to: 'css/[name].[ext]' },
-      { from: path.join(bootstrapPath, '/dist/css/bootstrap-theme.min.css'), to: 'css/[name].[ext]' },
-      { from: path.join(bootstrapPath, '/dist/css/bootstrap-theme.min.css.map'), to: 'css/[name].[ext]' },
+      {
+        from: path.join(bootstrapPath, "/dist/fonts/*"),
+        to: "fonts/[name].[ext]",
+      },
+      {
+        from: path.join(bootstrapPath, "/dist/css/bootstrap.min.css"),
+        to: "css/[name].[ext]",
+      },
+      {
+        from: path.join(bootstrapPath, "/dist/css/bootstrap.min.css.map"),
+        to: "css/[name].[ext]",
+      },
+      {
+        from: path.join(bootstrapPath, "/dist/css/bootstrap-theme.min.css"),
+        to: "css/[name].[ext]",
+      },
+      {
+        from: path.join(bootstrapPath, "/dist/css/bootstrap-theme.min.css.map"),
+        to: "css/[name].[ext]",
+      },
     ]),
 
-    new AssetsPlugin({ filename: 'build-assets.json' }),
+    new AssetsPlugin({ filename: "build-assets.json" }),
   ].filter(n => !!n),
 };
